b3b82458804187b4eb5ea644caa540cfdc4abb50
EchoCoder20 <albusaidi1915@gmail.com>
2025-07-07 08:57:50 +0400

    Reverse string task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[] names = {"Nasser", "Haitham", "Manal", "Hanin"};
+        System.out.println(Arrays.toString(names));
+        reverseArrayString(names);
+    }
+    public static void reverseArrayString(String [] arrayString)
+    {
+        StringBuffer stringBuffer = new StringBuffer();
+        List<StringBuffer> stringList=new ArrayList<>();
+        int lastIndex=arrayString.length-1;
+        int count=0;
+//
+        for (String names:  arrayString)
+        {
+            char[] charArray = names.toCharArray();
+            int lengthOfName=names.length();
+            int lastIndexOfName=lengthOfName-1;
+            count++;
+            for(int i=lastIndexOfName;i>-1;i--)
+                stringBuffer.append(names.charAt(i));
+            if(count<=lastIndex)
+             stringBuffer.append(",");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
         }
+        stringList.add(stringBuffer);
+        System.out.print(stringList);
     }
 }
